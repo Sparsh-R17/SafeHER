7ab3debc0ed59f0d147101af6cf5fc5a6f644c95
Sparsh-R17 <sparsh170702@gmail.com>
2023-03-06 15:03:25 +0530

    Sensor Events Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,6 +16,122 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const MaterialApp(
+      title: 'KAVACH',
+      home: ValueScreen(),
+    );
+  }
+}
+
+class ValueScreen extends StatefulWidget {
+  const ValueScreen({super.key});
+
+  @override
+  State<ValueScreen> createState() => _ValueScreenState();
+}
+
+class _ValueScreenState extends State<ValueScreen> {
+  bool _valueRun = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _valueRun
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  StreamBuilder<AccelerometerEvent>(
+                    stream: SensorsPlatform.instance.accelerometerEvents,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const CircularProgressIndicator();
+                      }
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Accelerometer Reading',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text(' X : ${snapshot.data!.x.toString()}'),
+                          Text(' Y : ${snapshot.data!.y.toString()}'),
+                          Text(' Z : ${snapshot.data!.z.toString()}'),
+                        ],
+                      );
+                    },
+                  ),
+                  StreamBuilder<GyroscopeEvent>(
+                    stream: SensorsPlatform.instance.gyroscopeEvents,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const CircularProgressIndicator();
+                      }
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Gyroscope Reading',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text(' X : ${snapshot.data!.x.toString()}'),
+                          Text(' Y : ${snapshot.data!.y.toString()}'),
+                          Text(' Z : ${snapshot.data!.z.toString()}'),
+                        ],
+                      );
+                    },
+                  ),
+                  StreamBuilder<MagnetometerEvent>(
+                    stream: SensorsPlatform.instance.magnetometerEvents,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const CircularProgressIndicator();
+                      }
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Magnetometer Reading',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text(' X : ${snapshot.data!.x.toString()}'),
+                          Text(' Y : ${snapshot.data!.y.toString()}'),
+                          Text(' Z : ${snapshot.data!.z.toString()}'),
+                        ],
+                      );
+                    },
+                  ),
+                ],
+              )
+            : const Text(
+                'Press Start',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                ),
+              ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _valueRun = !_valueRun;
+          });
+        },
+        child: Icon(
+          _valueRun ? Icons.stop : Icons.play_arrow,
+        ),
+      ),
+    );
   }
 }
