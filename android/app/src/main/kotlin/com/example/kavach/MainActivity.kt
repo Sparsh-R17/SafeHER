2169b47aa561bf295a7b54ec338a1e4019006d40
Sparsh-R17 <sparsh170702@gmail.com>
2023-03-06 14:32:37 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/kavach/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kavach
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+    (value) => runApp(
+      const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
